6135436cca88a08b9f67670d3b557435ef5744e0
luiscgl29 <luiscarlopz@gmail.com>
2025-08-29 10:24:35 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import empleadosRutas from "./routes/empleados.js";
+import { opcionesCors } from "./config/corsConfig.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors(opcionesCors));
 app.use(express.json());
 app.use(express.urlencoded());
 
